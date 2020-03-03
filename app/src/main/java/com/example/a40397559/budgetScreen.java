c7b2c3ad39fa1845ade12e4c509079159b6f92c9
Veonms <40397559@live.napier.ac.uk>
2020-03-03 16:25:00 +0000

    Created expandable lists

--- app/src/main/java/com/example/a40397559/budgetScreen.java
+++ app/src/main/java/com/example/a40397559/budgetScreen.java
@@ -3,10 +3,19 @@ package com.example.a40397559;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.ExpandableListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class budgetScreen extends AppCompatActivity {
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+    MainAdapter adapter;
 
     float x1, x2, y1, y2;
 
@@ -14,6 +23,36 @@ public class budgetScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_screen);
+
+        expandableListView = findViewById(R.id.expandableList);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new MainAdapter(this, listGroup, listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+    }
+
+    private void initListData() {
+        listGroup.add(getString(R.string.group1));
+        listGroup.add(getString(R.string.group2));
+
+        String[] array;
+
+        List<String> list = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group1);
+        for (String item : array) {
+            list.add(item);
+        }
+
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group2);
+        for (String item : array) {
+            list2.add(item);
+        }
+
+        listItem.put(listGroup.get(0), list);
+        listItem.put(listGroup.get(1), list2);
+        adapter.notifyDataSetChanged();
     }
 
     public boolean onTouchEvent(MotionEvent touchEvent) {
