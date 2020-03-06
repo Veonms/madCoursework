e51175658ccf25380be6c6d7c0f0ec0643665602
Veonms <40397559@live.napier.ac.uk>
2020-03-06 15:17:06 +0000

    Re-started clear button

--- app/src/main/java/com/example/a40397559/editScreen.java
+++ app/src/main/java/com/example/a40397559/editScreen.java
@@ -1,23 +1,13 @@
 package com.example.a40397559;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class editScreen extends AppCompatActivity {
 
     float x1, x2, y1, y2;
-
-    OnClickListener() {
-        @Override
-        public void onClick (View v){
-            txt_budgetEatingOut.getText().clear();
-        }
-    }
 
     public boolean onTouchEvent(MotionEvent touchEvent) {
         switch (touchEvent.getAction()) {
@@ -36,13 +26,5 @@ public class editScreen extends AppCompatActivity {
         }
         return false;
     }
-
-    reset.setOnClickListener(new
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_screen);
-        Button reset = (Button) findViewById(R.id.btn_reset);
-    });
 }
 
