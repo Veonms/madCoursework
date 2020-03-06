3bb62f494a7dc00ec562d701e150da08fac478fd
Veonms <40397559@live.napier.ac.uk>
2020-03-06 15:15:24 +0000

    Started clear button

--- app/src/main/java/com/example/a40397559/editScreen.java
+++ app/src/main/java/com/example/a40397559/editScreen.java
@@ -3,6 +3,8 @@ package com.example.a40397559;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +12,11 @@ public class editScreen extends AppCompatActivity {
 
     float x1, x2, y1, y2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_screen);
+    OnClickListener() {
+        @Override
+        public void onClick (View v){
+            txt_budgetEatingOut.getText().clear();
+        }
     }
 
     public boolean onTouchEvent(MotionEvent touchEvent) {
@@ -33,5 +36,14 @@ public class editScreen extends AppCompatActivity {
         }
         return false;
     }
+
+    reset.setOnClickListener(new
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_screen);
+        Button reset = (Button) findViewById(R.id.btn_reset);
+    });
 }
 
