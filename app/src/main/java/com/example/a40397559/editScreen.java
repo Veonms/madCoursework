49d21331f7c22de67ac17142b1a84e56ab34190f
Veonms <40397559@live.napier.ac.uk>
2020-03-10 14:14:18 +0000

    Clear button now clears all editText boxes in the activity_edit_screen xml file

--- app/src/main/java/com/example/a40397559/editScreen.java
+++ app/src/main/java/com/example/a40397559/editScreen.java
@@ -1,13 +1,47 @@
 package com.example.a40397559;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class editScreen extends AppCompatActivity {
 
     float x1, x2, y1, y2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_screen);
+
+        Button Clear = findViewById(R.id.btn_reset);
+
+        Clear.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                clearForm((ViewGroup) findViewById(R.id.editScreenXML));
+            }
+        });
+    }
+
+    private void clearForm(ViewGroup group) {
+        for (int i = 0, count = group.getChildCount(); i < count; ++i) {
+            View view = group.getChildAt(i);
+            if (view instanceof EditText) {
+                ((EditText) view).setText("");
+            }
+
+            if (view instanceof ViewGroup && (((ViewGroup) view).getChildCount() > 0))
+                clearForm((ViewGroup) view);
+        }
+    }
 
     public boolean onTouchEvent(MotionEvent touchEvent) {
         switch (touchEvent.getAction()) {
