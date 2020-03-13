57d847e46df5e96b7c9eaa7693195ba5b17f1b21
Veonms <40397559@live.napier.ac.uk>
2020-03-13 12:59:33 +0000

    Added expandable list with animation. Can add and remove items from the list.

--- app/src/main/java/com/example/a40397559/budgetScreen.java
+++ app/src/main/java/com/example/a40397559/budgetScreen.java
@@ -1,35 +1,110 @@
 package com.example.a40397559;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.widget.ExpandableListView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import com.diegodobelo.expandingview.ExpandingItem;
+import com.diegodobelo.expandingview.ExpandingList;
 
 public class budgetScreen extends AppCompatActivity {
-    ExpandableListView expandableListView;
-    List<String> listGroup;
-    HashMap<String, List<String>> listItem;
-    MainAdapter adapter;
 
     float x1, x2, y1, y2;
+    private ExpandingList mExpandingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_screen);
 
-        expandableListView = findViewById(R.id.expandableList);
-        listGroup = new ArrayList<>();
-        listItem = new HashMap<>();
-        adapter = new MainAdapter(this, listGroup, listItem);
-        expandableListView.setAdapter(adapter);
-        initListData();
+        mExpandingList = findViewById(R.id.expanding_list_main);
+        createItems();
+    }
+
+    private void createItems() {
+        addItem("Eating Out", new String[]{"House", "Boat", "Candy", "Collection", "Sport", "Ball", "Head"}, R.color.blue, R.drawable.ic_icon);
+        addItem("Entertainment", new String[]{"Dog", "Horse", "Boat"}, R.color.blue, R.drawable.ic_icon);
+        addItem("Expenses", new String[]{"Cat"}, R.color.blue, R.drawable.ic_icon);
+        addItem("Groceries", new String[]{"Parrot", "Elephant", "Coffee"}, R.color.blue, R.drawable.ic_icon);
+        addItem("Shopping", new String[]{}, R.color.blue, R.drawable.ic_icon);
+    }
+
+    private void addItem(String title, String[] subItems, int colorRes, int iconRes) {
+        //Let's create an item with R.layout.expanding_layout
+        final ExpandingItem item = mExpandingList.createNewItem(R.layout.expanding_layout); //ERROR
+
+        //If item creation is successful, let's configure it
+        if (item != null) {
+            item.setIndicatorColorRes(colorRes);
+            item.setIndicatorIconRes(iconRes);
+            //It is possible to get any view inside the inflated layout. Let's set the text in the item
+            ((TextView) item.findViewById(R.id.title)).setText(title);
+
+            //We can create items in batch.
+            item.createSubItems(subItems.length);
+            for (int i = 0; i < item.getSubItemsCount(); i++) {
+                //Let's get the created sub item by its index
+                final View view = item.getSubItemView(i);
+
+                //Let's set some values in
+                configureSubItem(item, view, subItems[i]);
+            }
+            item.findViewById(R.id.add_more_sub_items).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showInsertDialog(new OnItemCreated() {
+                        @Override
+                        public void itemCreated(String title) {
+                            View newSubItem = item.createSubItem();
+                            configureSubItem(item, newSubItem, title);
+                        }
+                    });
+                }
+            });
+
+            item.findViewById(R.id.remove_item).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExpandingList.removeItem(item);
+                }
+            });
+        }
+    }
+
+    private void configureSubItem(final ExpandingItem item, final View view, String subTitle) {
+        ((TextView) view.findViewById(R.id.sub_title)).setText(subTitle);
+        view.findViewById(R.id.remove_sub_item).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item.removeSubItem(view);
+            }
+        });
+    }
+
+    private void showInsertDialog(final OnItemCreated positive) {
+        final EditText text = new EditText(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(text);
+        builder.setTitle(R.string.enter_title);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                positive.itemCreated(text.getText().toString());
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.show();
+    }
+
+    interface OnItemCreated {
+        void itemCreated(String title);
     }
 
     public boolean onTouchEvent(MotionEvent touchEvent) {
@@ -50,50 +125,4 @@ public class budgetScreen extends AppCompatActivity {
         return false;
     }
 
-    private void initListData() {
-        listGroup.add(getString(R.string.EatingOut));
-        listGroup.add(getString(R.string.Entertainment));
-        listGroup.add(getString(R.string.Expenses));
-        listGroup.add(getString(R.string.Groceries));
-        listGroup.add(getString(R.string.Shopping));
-
-        String[] array;
-
-        List<String> eatingOutList = new ArrayList<>();
-        array = getResources().getStringArray(R.array.EatingOut);
-        for (String item : array) {
-            eatingOutList.add(item);
-        }
-
-        List<String> entertainmentList = new ArrayList<>();
-        array = getResources().getStringArray(R.array.Entertainment);
-        for (String item : array) {
-            entertainmentList.add(item);
-        }
-
-        List<String> expensesList = new ArrayList<>();
-        array = getResources().getStringArray(R.array.Expenses);
-        for (String item : array) {
-            expensesList.add(item);
-        }
-
-        List<String> groceriesList = new ArrayList<>();
-        array = getResources().getStringArray(R.array.Groceries);
-        for (String item : array) {
-            groceriesList.add(item);
-        }
-
-        List<String> shoppingList = new ArrayList<>();
-        array = getResources().getStringArray(R.array.Shopping);
-        for (String item : array) {
-            shoppingList.add(item);
-        }
-
-        listItem.put(listGroup.get(0), eatingOutList);
-        listItem.put(listGroup.get(1), entertainmentList);
-        listItem.put(listGroup.get(2), expensesList);
-        listItem.put(listGroup.get(3), groceriesList);
-        listItem.put(listGroup.get(4), shoppingList);
-        adapter.notifyDataSetChanged();
-    }
 }
