3192881b142968ca746a2ecc0e17c056207388f0
Veonms <40397559@live.napier.ac.uk>
2020-03-14 12:17:36 +0000

    Commented MainActivity.java

--- app/src/main/java/com/example/a40397559/MainActivity.java
+++ app/src/main/java/com/example/a40397559/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+/*
+* This activity will display for 3 (3000ms) seconds before changing the activity to
+* HomeActivity.java.
+ */
 public class MainActivity extends AppCompatActivity {
-    private static int splashTimeout = 2000;
+
+    private static int splashTimeout = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // Creates intent to change to a new activity
                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                // Changes to new activity
                 startActivity(homeIntent);
                 finish();
             }
